6e8e2584d8ca5da506152cfa30a9aaf881f96847
studsarah <38938582+studsarah@users.noreply.github.com>
2019-06-18 16:47:34 +0200

    Create seq_depth.R

---
+++ seq_depth.R
@@ -0,0 +1,15 @@
+# Estimate sequencing depth
+
+#set the wanted phyloseq object: ps_16S/ITS_seed/soil/germ/elev
+ps=ps_16S_seed
+
+sdt <- data.table(as(sample_data(ps), "data.frame"),
+                  TotalReads = sample_sums(ps), 
+                  keep.rownames = TRUE)
+
+pSeqDepth <- ggplot(sdt, aes(TotalReads)) + 
+  geom_histogram() + 
+  ggtitle("Sequencing Depth")
+
+pSeqDepth + labs(x="Sample size (read total)",y= "Number of samples") +
+  scale_x_log10()
