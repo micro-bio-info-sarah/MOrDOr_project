a56aec5b728176ca753daff5716dba1320d7b421
studsarah <38938582+studsarah@users.noreply.github.com>
2019-06-19 10:05:53 +0200

    Update Beta_div.R

--- Beta_div.R
+++ Beta_div.R
@@ -1,4 +1,8 @@
 # Estimate beta-diversity
+require(phyloseq)
+require(ggplot2)
+require(vegan)
+require(data.table)
 
 #set the wanted phyloseq object: ps_16S/ITS_seed/soil/germ/elev
 ps=ps_16S_seed
