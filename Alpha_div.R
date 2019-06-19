2495c62b9178b1ee0b2b47818731db137f9add7b
studsarah <38938582+studsarah@users.noreply.github.com>
2019-06-19 09:59:15 +0200

    Update Alpha_div.R

--- Alpha_div.R
+++ Alpha_div.R
@@ -1,45 +1,11 @@
-Estimate alpha-diversity
+# Estimate alpha-diversity
+require(phyloseq)
+require(ggplot2)
 
 #set the wanted phyloseq object: ps_16S/ITS_seed/soil/germ/elev
 ps=ps_16S_seed
 
 ### Rarefaction ----
-
-require(gridBase)
-
-rarecurve2 <- function (x, step = 1, sample, xlab = "Sample Size", ylab = "Species", label = TRUE, col = "black", ...)
-  ## See documentation for vegan rarecurve, col is now used to define
-  ## custom colors for lines and panels
-{
-  tot <- rowSums(x)
-  S <- specnumber(x)
-  nr <- nrow(x)
-  out <- lapply(seq_len(nr), function(i) {
-    n <- seq(1, tot[i], by = step)
-    if (n[length(n)] != tot[i]) 
-      n <- c(n, tot[i])
-    drop(rarefy(x[i, ], n))
-  })
-  Nmax <- sapply(out, function(x) max(attr(x, "Subsample")))
-  Smax <- sapply(out, max)
-  plot(c(1, max(Nmax)), c(1, max(Smax)), xlab = xlab, ylab = ylab, 
-       type = "n", ...)
-  if (!missing(sample)) {
-    abline(v = sample)
-    rare <- sapply(out, function(z) approx(x = attr(z, "Subsample"), 
-                                           y = z, xout = sample, rule = 1)$y)
-    abline(h = rare, lwd = 0.5)
-  }
-  for (ln in seq_len(length(out))) {
-    color <- col[((ln-1) %% length(col)) + 1]
-    N <- attr(out[[ln]], "Subsample")
-    lines(N, out[[ln]], col = color, ...)
-  }
-  if (label) {
-    ordilabel(cbind(tot, S), labels = rownames(x), col = col, ...)
-  }
-  invisible(out)
-}
 
 #rarefaction curve
 rarecurve2(t(otu_table(ps)), step=100,
