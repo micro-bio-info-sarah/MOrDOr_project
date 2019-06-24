9fdb4cb2bd80a5ca26d8b66b4f568602a8f2cbb1
studsarah <38938582+studsarah@users.noreply.github.com>
2019-06-24 10:03:43 +0200

    Update MOrDOr_functions.R

--- MOrDOr_functions.R
+++ MOrDOr_functions.R
@@ -74,3 +74,40 @@ plot_composition <- function(physeq, taxaRank1 = "Phylum", taxaSet1 = "Proteobac
   p <- p + ggtitle(paste("Composition within", taxaSet1, "(", numberOfTaxa, "top", taxaRank2, ")"))
   return(p)
 }
+
+### Extract bootstraps for dendrograms ----
+#https://stackoverflow.com/questions/22749634/how-to-append-bootstrapped-values-of-clusters-tree-nodes-in-newick-format-in
+as.phylo.hclust.with.nodenames <- function (x, nodenames, ...) #We add a nodenames argument
+{
+  N <- dim(x$merge)[1]
+  edge <- matrix(0L, 2 * N, 2)
+  edge.length <- numeric(2 * N)
+  node <- integer(N)
+  node[N] <- N + 2L
+  cur.nod <- N + 3L
+  j <- 1L
+  for (i in N:1) {
+    edge[j:(j + 1), 1] <- node[i]
+    for (l in 1:2) {
+      k <- j + l - 1L
+      y <- x$merge[i, l]
+      if (y > 0) {
+        edge[k, 2] <- node[y] <- cur.nod
+        cur.nod <- cur.nod + 1L
+        edge.length[k] <- x$height[i] - x$height[y]
+      }
+      else {
+        edge[k, 2] <- -y
+        edge.length[k] <- x$height[i]
+      }
+    }
+    j <- j + 2L
+  }
+  if (is.null(x$labels)) 
+    x$labels <- as.character(1:(N + 1))
+  node.lab <- nodenames[order(node)] #Here we define our node labels
+  obj <- list(edge = edge, edge.length = edge.length/2, tip.label = x$labels, 
+              Nnode = N, node.label = node.lab) #And you put them in the final object
+  class(obj) <- "phylo"
+  reorder(obj)
+}
